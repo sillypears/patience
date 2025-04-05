e3726526ddbadd413a76a686bac2aadacaaf2452
sillypears <sillypears@users.noreply.github.com>
2025-04-05 09:10:59 -0400

    another saturday

--- scenes/main/intro.gd
+++ scenes/main/intro.gd
@@ -1,11 +1,31 @@
 extends Control
 
+const WORD1 := "MADE"
+const WORD2 := "WITH"
+const WORD3 := "AI"
+var WORDS = "MADE WITH\nCONNECTIVE TISSUE OF\nSENTIENT AI LIFEFORMS"
+var WORD_COUNTER = 0
+
+@onready var ai: Label = $Margin/Box/BoxTop/AI
+@onready var timer: Timer = $Timer
+@onready var timer2: Timer = $Timer2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	timer.wait_time = 0.1
+	timer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if WORD_COUNTER >= len(WORDS):
+		timer.stop()
+		set_process(false)
+		timer2.start()
+
+func _on_timer_timeout() -> void:
+	ai.text += WORDS[WORD_COUNTER]
+	WORD_COUNTER += 1
+
+
+func _on_timer_2_timeout() -> void:
+	SceneManager.change_scene("res://scenes/main/main.tscn")
--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,11 +1,27 @@
 extends Control
 
+@onready var scaler: Timer = $Scaler
+@onready var title: Label = $Margin/BoxContainer/Vbox/Title
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	await get_tree().process_frame
+	title.pivot_offset = title.size / 2
+	
 func _process(delta: float) -> void:
-	pass
+	var scale_factor = 1.0 + sin(Time.get_ticks_msec() / 1000.0 * 1.5) * 0.1
+	title.scale = Vector2.ONE * scale_factor
+		
+func _on_scaler_timeout() -> void:
+	title.scale = Vector2(1, 1)
+
+
+func _on_start_pressed() -> void:
+	var t = Timer.new()
+	t.wait_time = 0.75
+	t.timeout.connect(_go_to_game)
+	add_child(t)
+	t.start()
+	
+func _go_to_game() -> void:
+	SceneManager.change_scene("res://scenes/game/game.tscn")
+	
